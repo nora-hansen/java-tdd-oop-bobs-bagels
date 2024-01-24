6adde0219f835bd17bff40c5b087d4496ec7e86e
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-24 12:43:16 +0100

    Gets info from inbound sms

--- src/main/java/com/booleanuk/extension/SMSInbound.java
+++ src/main/java/com/booleanuk/extension/SMSInbound.java
@@ -6,11 +6,24 @@ import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class SMSInbound {
     public static void main(String[] args)
     {
-        Body b = new Body.Builder("boob").build();
+        // Pattern p = Pattern.compile(".*&Body=(.*)&FromCountry");
+
         post("/receive-sms", (req, res) -> {
+            //Matcher m = p.matcher(req.body());
+            //if(m.find())
+            //{
+            //    placeSMSOrder(m.group(1));
+            //}
+
+            Body b = new Body.Builder("What's did you say? I don't have ears... Was it " + req.queryParams("Body") + "?").build();
+
             Message sms = new Message.Builder()
                     .body(b)
                     .build();
@@ -19,5 +32,12 @@ public class SMSInbound {
                     .build();
             return twiml.toXml();
         });
+    }
+
+    public static boolean placeSMSOrder(String requestBody)
+    {
+        String[] values = requestBody.split("\\+");
+        System.out.println("Message contains " + Arrays.toString(values));
+        return true;
     }
 }
